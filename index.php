04d5f7b916be4f5db300dea49b69415bb9ee3e0c
ana4tn8evituvheh <email@example.com>
2023-12-21 23:56:16 -0300

    Homepage estática

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>UP Weapon Builder</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body class="body">
+
+    <div class="centralizar">
+        <div class="wrapper"></div>
+    </div>
+
+    <div class="parent position">
+        <div class="div1 position titulo"><span class="tituloct">ARQUEARIA</span> </div>
+        <div class="div2 position"><img src="imgs/arco.webp" class="imagemcontainers imgarco"> </div>
+        <div class="div3 position descricao"> Armas para ataque a distância</div>
+
+        <div class="div4 position titulo"> <span class="tituloct">MARCIAIS</span> </div>
+        <div class="div5 position"> 
+        <img src="imgs/espada.png" class="imagemcontainers imgespada"> 
+        </div>
+        <div class="div6 position descricao"> Armas para ataque corpo-a-corpo </div>
+
+        <div class="div7 position titulo"> <span class="tituloct">BALÍSTICAS</span> </div>
+        <div class="div8 position"> <img src="imgs/arma.png" class="imagemcontainers imgarma"> </div>
+        <div class="div9 position descricao"> Armas para ataque a distância<br> e de grande poder </div>
+
+        <div class="div10 position titulo"> <span class="tituloct">ARTEFATOS</span> </div>
+        <div class="div11 position"> <img src="imgs/artefato.png" class="imagemcontainers imgartefato"> </div>
+        <div class="div12 position descricao"> Artefatos para magia </div>
+</div>
+        
+
+
+    
+    
+    
+    
+</body>
+</html>
